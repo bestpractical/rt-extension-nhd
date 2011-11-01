20c78f0b4b783f5dfc3fadb6be74e40e3f71988a
Ruslan Zakirov <ruz@bestpractical.com>
2011-11-01 15:46:08 +0400

    custom NewItem method required

--- lib/RT/NHD/Agreements.pm
+++ lib/RT/NHD/Agreements.pm
@@ -8,6 +8,11 @@ use RT::NHD::Agreement;
 
 sub Table { 'NHDAgreements' }
 
+sub NewItem {
+    my $self = shift;
+    return RT::NHD::Agreement->new( $self->CurrentUser );
+}
+
 RT::Base->_ImportOverlays();
 
 1;
